85047cde5552f24fe6e904aa8bbecf52fe3fb1a8
vineshSwargam <vinesh@therealbrokerage.com>
2023-06-21 23:07:10 +0530

    form

---
+++ components/Form.tsx
@@ -0,0 +1,80 @@
+import { PostForm } from '@app/create-post/page';
+import '@styles/globals.css';
+import Link from 'next/link';
+import React from 'react';
+
+
+interface FormProps {
+  type: string;
+  post: PostForm;
+  setPost: React.Dispatch<React.SetStateAction<PostForm>>
+  submitting: boolean;
+  handleSubmit(e: any): void;
+}
+
+const Form: React.FC<FormProps> = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{type} Post</span>
+      </h1>
+      <p className='desc text-left max-w-md'>
+        {type} and share your love for a song, singer,
+        song-writer, or producer, and add links to where others can listen to
+        them!
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
+      >
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Your Post
+          </span>
+
+          <textarea
+            value={post.content}
+            onChange={(e) => setPost({ ...post, content: e.target.value })}
+            placeholder='Write your post here'
+            required
+            className='form_textarea '
+          />
+        </label>
+
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Relevant Hashtags{" "}
+            <span className='font-normal'>
+              (#rahman, #queen, #bollywood, etc.)
+            </span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            type='text'
+            placeholder='#Tag'
+            required
+            className='form_input'
+          />
+        </label>
+
+        <div className='flex-end mx-3 mb-5 gap-4'>
+          <Link href='/' className='text-gray-500 text-sm'>
+            Cancel
+          </Link>
+
+          <button
+            type='submit'
+            disabled={submitting}
+            className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'
+          >
+            {submitting ? `${type}ing...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
